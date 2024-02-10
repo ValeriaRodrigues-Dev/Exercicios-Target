e8b70c0ce3c3d7704babed03345cd0982f51e40d
ValeriaRodrigues-Dev <114243387+ValeriaRodrigues-Dev@users.noreply.github.com>
2024-02-10 19:22:04 -0300

    subindoex3

--- exercicio 3/fatdiario.js
+++ exercicio 3/fatdiario.js
@@ -1,16 +1,21 @@
-fetch('dados.json')
-  .then(response => response.json())
+fetch('dados.json').then(response => response.json())
   .then(data => {
     const faturamentoSemZero = data.filter(dia => dia.valor !== 0);
     const mediaMensal = faturamentoSemZero.reduce((total, dia) => total + dia.valor, 0) / faturamentoSemZero.length;
     const valoresFaturamento = faturamentoSemZero.map(dia => dia.valor);
     const menorFaturamento = Math.min(...valoresFaturamento);
-    
+
     const maiorFaturamento = Math.max(...valoresFaturamento);
     const diasAcimaDaMedia = faturamentoSemZero.filter(dia => dia.valor > mediaMensal).length;
+   
 
+    console.log("-------------------------------------------------")
     console.log("Menor faturamento diário:", menorFaturamento);
+    
+    console.log("-------------------------------------------------")
     console.log("Maior faturamento diário:", maiorFaturamento);
+    
+    console.log("-------------------------------------------------")
     console.log("Número de dias com faturamento acima da média mensal:", diasAcimaDaMedia);
   })
   .catch(error => console.error('Erro ao carregar os dados:', error));
